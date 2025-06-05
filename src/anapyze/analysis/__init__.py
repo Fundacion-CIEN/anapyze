b5730bed92462d09d9a8a26cae2cd1a56c1bed56
Orfeas23535 <ovourkas@fundacioncien.es>
2025-06-05 14:59:54 +0200

    Update __init__2.py

--- src/anapyze/analysis/__init__.py
+++ src/anapyze/analysis/__init__.py
@@ -5,7 +5,8 @@ Statistical analysis routines for anapyze.
 from .two_samples import (
     run_2sample_ttest_spm,
     run_2sample_ttest_cat12_new_tiv_model,
-    run_2sample_ttest_atlas,
+    run_2sample_ttest_atlas
+    run_2sample_anova_with_covariate_atlas,
 )
 from .correlations import (
     voxel_wise_corr_images_vs_scale,
@@ -20,4 +21,5 @@ __all__ = [
     'voxel_wise_corr_images_vs_scale',
     'image_to_image_corr_atlas_based_spearman',
     'normalized_cross_correlation_2images'
+    ' run_2sample_anova_with_covariate_atlas'
 ]
